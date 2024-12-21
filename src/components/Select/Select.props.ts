b1a69675449b5cb06b69f4da0af7f7caba8f71e8
Yury S <yurafikys@gmail.com>
2024-12-21 17:08:00 +0300

    refactor: Component Select

--- src/components/Select/Select.props.ts
+++ src/components/Select/Select.props.ts
@@ -1,5 +1,10 @@
 import { HTMLAttributes, ReactNode } from 'react';
 
+export interface OptionProps {
+	value: string;
+	label: string;
+}
+
 export interface SelectProps extends HTMLAttributes<HTMLSelectElement> {
 	children?: ReactNode;
 }
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,22 +1,36 @@
+import { ChangeEvent, useState } from 'react';
 import styles from './Select.module.css';
-import { SelectProps } from './Select.props';
+import { OptionProps, SelectProps } from './Select.props';
 
 function Select({ children, ...props }: SelectProps) {
+	const [selectedOption, setSelectedOption] = useState<string>('');
+
+	const options: OptionProps[] = [
+		{ value: 'Tasma 100', label: 'Tasma 100' },
+		{ value: 'Kodak Vision 500', label: 'Kodak Vision 500' },
+		{ value: 'Hitchcock 250', label: 'Hitchcock 250' },
+	];
+
+	const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+		setSelectedOption(event.target.value);
+	};
+
 	return (
 		<div className={styles['select-wrapper']}>
-			<select className={styles.select} {...props}>
-				<option disabled selected hidden>
+			<select
+				className={styles.select}
+				value={selectedOption}
+				onChange={handleChange}
+				{...props}
+			>
+				<option value='' disabled selected hidden>
 					выберите плёнку...
 				</option>
-				<option className={styles.option} value='Tasma 100'>
-					Tasma 100
-				</option>
-				<option className={styles.option} value='Kodak Vision 500'>
-					Kodak Vision 500
-				</option>
-				<option className={styles.option} value='Hitchcock 250'>
-					Hitchcock 250
-				</option>
+				{options.map(({ value, label }) => (
+					<option key={value} value={value}>
+						{label}
+					</option>
+				))}
 				{children}
 			</select>
 		</div>
