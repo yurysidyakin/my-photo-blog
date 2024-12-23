f09b46b995b36928e72e1b7ffd221e3d7823f8c0
Yury S <yurafikys@gmail.com>
2024-12-23 19:41:28 +0300

    style: Component PhotoCard

--- src/pages/PhotoCard/Photo.tsx
+++ src/pages/PhotoCard/Photo.tsx
@@ -1,5 +1,4 @@
 import { Link, useParams } from 'react-router-dom';
-import Paragraph from '../../components/Paragraph/Paragraph';
 import { photodb } from '../../db/photodb';
 import styles from './Photo.module.css';
 import { PhotoProps } from './Photo.props';
@@ -18,12 +17,32 @@ function Photo({ ...props }: PhotoProps): JSX.Element {
 				назад...
 			</Link>
 			<img className={styles['photo']} src={photo.path} alt='photo' />
-			<Paragraph>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ex deserunt
-				repellat ad, debitis sunt sint, eveniet numquam id nobis laudantium
-				veritatis culpa reiciendis, vero molestias blanditiis tempore quis
-				assumenda.
-			</Paragraph>
+			<div className={styles['photo_desc']}>
+				<div className={styles['photo_item']}>
+					<img
+						className={styles['icon']}
+						src='/public/icon/camera-icon.png'
+						alt='camera'
+					/>
+					<div>{photo.camera}</div>
+				</div>
+				<div className={styles['photo_item']}>
+					<img
+						className={styles['icon']}
+						src='/public/icon/plenka-icon.png'
+						alt=''
+					/>
+					<div>{photo.plenka}</div>
+				</div>
+				<div className={styles['photo_item']}>
+					<img
+						className={styles['icon']}
+						src='/public/icon/location-icon.png'
+						alt='location'
+					/>
+					<div className={styles['photo_text']}>{photo.location}</div>
+				</div>
+			</div>
 		</div>
 	);
 }
