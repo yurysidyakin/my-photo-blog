ecb999aede8657adbd11388a453fcf3af75b89a5
Yury S <yurafikys@gmail.com>
2024-12-29 15:33:41 +0300

    feat: Added counter for favorites photos

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,9 +1,13 @@
 import cn from 'classnames';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { RootState } from '../../store/store';
 import styles from './Menu.module.css';
 import { MenuProps } from './Menu.props';
 
 function Menu({ children }: MenuProps) {
+	const photos = useSelector((s: RootState) => s.favorite.photos);
+
 	return (
 		<nav className={styles.menu}>
 			<ul className={styles.list}>
@@ -19,7 +23,7 @@ function Menu({ children }: MenuProps) {
 						главная
 					</NavLink>
 				</li>
-				<li>
+				<li className={styles.item}>
 					<NavLink
 						className={({ isActive }) =>
 							cn(styles.link, {
@@ -30,6 +34,10 @@ function Menu({ children }: MenuProps) {
 					>
 						избранное
 					</NavLink>
+					{photos.length == 0 && ''}
+					{photos.length !== 0 && (
+						<div className={styles.count}>{photos.length}</div>
+					)}
 				</li>
 				<li>
 					<NavLink
@@ -43,9 +51,13 @@ function Menu({ children }: MenuProps) {
 						контакты
 					</NavLink>
 				</li>
-				<li className={styles.link}>
-					<img src='/public/icon/user-icon.svg' alt='user' />
-					<div>username</div>
+				<li className={styles.item}>
+					<img
+						className={styles.icon}
+						src='/public/icon/user-icon.svg'
+						alt='user'
+					/>
+					<div className={styles.name}>username</div>
 				</li>
 			</ul>
 			{children}
