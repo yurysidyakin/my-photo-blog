bb6d06c8d0ea492c08cb650a7730e1439c8268fc
Yury Sidyakin <yurafikys@gmail.com>
2025-06-22 20:24:48 +0300

    feat: Changed the static year value to a function

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,9 @@ function Footer({ children }: FooterProps): JSX.Element {
 		<footer className={styles.footer}>
 			<div className={styles["footer-content"]}>
 				<img className={styles["footer-logo"]} src="icons/logo-footer.svg" alt="footer-logo" />
-				<div className={styles["footer-text"]}>©2024. All rights reserved.</div>
+				<div className={styles["footer-text"]}>
+					© {new Date().getFullYear()}. All rights reserved.
+				</div>
 				<a href="https://github.com/fikyslsgg">
 					<img src="icons/github-icon.svg" alt="git-icon" />
 				</a>
